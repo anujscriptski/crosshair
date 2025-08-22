41ead491143f1cc68692fff4f68fbfb30fd906d7
Anuj <genesisnetworks28@gmail.com>
2025-08-22 15:07:19 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Anuj'
+description 'Custom Crosshair System for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles.css',
+    'html/script.js',
+    'html/crosshairs/*.png',
+    'settings.json'
+}
